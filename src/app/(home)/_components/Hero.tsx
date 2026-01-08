cdfa25b189ab4cffe9353606a6bac11d84dc5e9e
Alvin Dennis <alvindennis80@gmail.com>
2026-01-08 20:41:12 +0530

    feat: change hero image to local

--- src/app/(home)/_components/Hero.tsx
+++ src/app/(home)/_components/Hero.tsx
@@ -6,7 +6,6 @@ import { MotionDiv, MotionH1, MotionHeader, MotionP } from "@/components/MuFrame
 import MuImage from "@/components/MuImage";
 import { Button } from "@/components/ui/button";
 import { useRedirectToApp } from "@/lib/utils";
-import { cdnUrl } from "@/services/cdn";
 
 const fadeInUp: Variants = {
   hidden: { opacity: 0, y: 50 },
@@ -95,7 +94,7 @@ export default function Hero() {
 
       <div className="relative w-full mt-auto flex justify-center items-end">
         <MuImage
-          src={cdnUrl("src/modules/Public/Home/assets/illustration.webp")}
+          src="/assets/illustration.webp"
           alt="Community illustration showcasing µLearn peer learning platform"
           className="object-cover object-bottom h-auto w-full max-w-screen md:max-w-[90vw] lg:max-w-[80vw]"
           preload
