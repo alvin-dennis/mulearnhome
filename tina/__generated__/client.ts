a1114dcb00783ccec21a3da80cb1dff9c53772dd
awindsr <awindsr@gmail.com>
2026-01-20 17:40:49 +0530

    feat: update tina cms config

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'D:/Dev/work/Mulearn/mulearnhome/tina/__generated__/.cache/1768907078085', url: 'https://content.tinajs.io/2.0/content/b9e54343-1f10-4879-aab6-19e5edff6315/github/production', token: '5bd62e8201809ede153211fb33e88c1654b0394e', queries,  });
+export const client = createClient({ cacheDir: 'D:/Dev/work/Mulearn/mulearnhome/tina/__generated__/.cache/1768910888434', url: 'https://content.tinajs.io/2.0/content/b9e54343-1f10-4879-aab6-19e5edff6315/github/production', token: '5bd62e8201809ede153211fb33e88c1654b0394e', queries,  });
 export default client;
   
