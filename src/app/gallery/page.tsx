12202c9760f9c7ec25d274acd4510f95fcad5841
Alvin Dennis <alvindennis80@gmail.com>
2026-05-01 11:41:39 +0530

    fix: padding on gallery page top

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -21,7 +21,7 @@ export default function GalleryPage() {
   };
 
   return (
-    <section className="min-h-screen">
+    <section className="px-6 py-8 min-h-screen">
       <div className="max-w-[1300px] mx-auto mb-16">
         <MotionDiv
           initial="hidden"
