11693a5eac8e2d6599c9712cb06ce5f8e3214138
Alvin <alvindennis80@gmail.com>
2026-09-01 21:59:54 +0530

    style(home): remove max-width constraint from roles section

--- src/features/home/components/roles.tsx
+++ src/features/home/components/roles.tsx
@@ -29,7 +29,7 @@ export function Roles() {
 
   return (
     <Section
-      className="max-w-[1200px] mx-auto"
+      className="mx-auto"
       variants={fadeInUp}
       initial="hidden"
       whileInView="visible"
